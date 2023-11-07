72852e5d7d6c2f1a37136ce4d4a6660cf36332e1
maituananh15 <taomalai987@gmail.com>
2023-11-07 20:56:30 +0700

    TVM

---
+++ TVM/Team.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.naruto').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+  });
